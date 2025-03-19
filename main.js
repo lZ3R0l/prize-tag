6093586a540e7551c6ebfec25c880a076229f97e
lZ3R0l <galaxyeye.1999@gmail.com>
2025-03-19 13:14:35 +0100

    added prize discounter

---
+++ main.js
@@ -0,0 +1,16 @@
+let prize
+let newprize
+let currency = ''
+
+prize = prompt('Enter prize: ')
+
+const match = prize.match(/[\u20A0-\u20CF$₹€£¥₩₦]/);
+if (match) {
+  currency = match[0]
+  prize = prize.replace(currency, '')
+}
+
+prize = Number(prize)
+newprize = prize * 0.9
+
+console.log(`New prize after 10% discount: ${currency}${newprize}`)
